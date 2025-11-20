8f871e39a5b394e226a06cddfb6e4a34489a358f
Björn Wege <bjoern.wege@web.de>
2025-11-20 12:44:58 +0100

    Grundlegende Funktion MPI

--- 06-einfuehrung-mpi/timempi.c
+++ 06-einfuehrung-mpi/timempi.c
@@ -1,12 +1,19 @@
 #define _DEFAULT_SOURCE
-
+// mpirun -np=4 ~/Uebungen/6/timempi.x
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
+#include <mpi.h> // Nutzung von MPI
 
-int main(void) {
+int main(int argc, char *argv[]){
+
+  //initialisiert die parallelen Prozesse und gibt jedem einen Rang(rank).
+  int size, rank;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &size); // Gesamtzahl der Prozesse
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Speicher den eigenen Rang
 
   struct timeval tv;
   time_t current_time;
@@ -22,10 +29,12 @@ int main(void) {
   micro_sec = tv.tv_usec;
 
   strftime(time_string, 30, "%Y-%m-%d %T", localtime(&current_time));
-  snprintf(output, 80, "[%d] %s // %s.%d", -1, hostname, time_string, micro_sec);
+  snprintf(output, 80, "[%d] %s // %s.%d", rank, hostname, time_string, micro_sec);
 
   printf("%s\n", output);
   printf("%d\n", micro_sec);
 
+  MPI_Finalize(); //Beendet MPI
+  
   return 0;
 }
